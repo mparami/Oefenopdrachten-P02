cec4ba01a08119028c889b3be7976ef4020f71fc
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2023-11-29 22:17:27 +0100

    opdracht 1 t/m 6

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-console.log("main is running...");
+// Opdr. 1, Opdr. 2
+let word = prompt("Raad een woord");
+while (word !== "VSCode") {
+	if (word === "quit") {
+		break;
+	}
 
-// Opdr. 1, opdr. 2
-// let word = prompt("Type iets in");
-// while (word !== "geheim") {
-// 	if (word === "quit") {
-// 		break;
-// 	}
-
-// 	alert("Verkeerd geraden, probeer nog eens");
-// 	word = prompt("Type weer iets in");
-// }
-// alert("Je hebt goed geraden");
+	alert("Foute woord geraden");
+	word = prompt("Raad een woord");
+}
+if (word !== "quit") {
+	alert("VSCode");
+}
 
 // Opdr. 3
-let temperatures = [3.6, 3.9, 6.5, 9.9, 13.4, 16.2, 18.3, 17.9, 14.7, 10.9, 7.0, 4.2];
+const temperatures = [3.6, 3.9, 6.5, 9.9, 13.4, 16.2, 18.3, 17.9, 14.7, 10.9, 7.0, 4.2];
 let highest = 0;
-let coldest = 100;
+let lowest = 100;
 for (let i = 0; i < temperatures.length; i++) {
 	const temp = temperatures[i];
 
-	// bepaal of temp hoger is dan highest
+	// Bepaal het hoogste getal
 	if (temp > highest) {
 		highest = temp;
 	}
-
-	// bepaal of temp lager is dan coldest
-	if (temp < coldest) {
-		coldest = temp;
+	// Bepaal het laagste getal
+	if (temp < lowest) {
+		lowest = temp;
 	}
 
+	// Toon temperaturen boven de 10 graden
 	if (temp > 10) {
-		console.log("Temperatuur hoger dan 10 graden: ", temp);
+		console.log("Groter dan 10: ", temp);
 	}
 }
 console.log("Hoogste temperatuur: ", highest);
-console.log("Laagste temperatuur: ", coldest);
+console.log("Laagste temperatuur: ", lowest);
 
 // Opdr. 4
-temperatures = [
+const list = [
 	{
 		temp: 3.6,
 		month: "januari",
@@ -50,49 +50,35 @@ temperatures = [
 		temp: 6.5,
 		month: "maart",
 	},
-	{
-		temp: 9.9,
-		month: "april",
-	},
-	{
-		temp: 13.4,
-		month: "mei",
-	},
-	{
-		temp: 16.2,
-		month: "juni",
-	},
-	{
-		temp: 18.3,
-		month: "juli",
-	},
-	{
-		temp: 17.9,
-		month: "augustus",
-	},
-	{
-		temp: 14.7,
-		month: "september",
-	},
-	{
-		temp: 10.9,
-		month: "oktober",
-	},
-	{
-		temp: 7.0,
-		month: "november",
-	},
-	{
-		temp: 4.2,
-		month: "december",
-	},
 ];
 
-const list = document.querySelector(".list");
-const table = document.querySelector("table");
-for (let i = 0; i < temperatures.length; i++) {
-	const temp = temperatures[i];
-	list.innerHTML += "<li>" + temp.month + ": " + temp.temp + "</li>";
+const ulList = document.querySelector(".list");
+for (let i = 0; i < list.length; i++) {
+	const item = list[i];
 
-	table.innerHTML += `<tr><td>${temp.month}</td><td>${temp.temp}</td></tr>`;
+	// ulList.innerHTML += "<li>" + item.month + ": " + item.temp + "</li>";
+	ulList.innerHTML += `<li>${item.month}: ${item.temp}</li>`;
 }
+
+// Opdr. 5
+const table = document.querySelector("table");
+for (let i = 0; i < list.length; i++) {
+	const item = list[i];
+
+	table.innerHTML += `
+    <tr>
+        <td>${item.month}</td>
+        <td>${item.temp}</td>
+    </tr>`;
+}
+
+// Opdr.6
+const container = document.querySelector(".container");
+const btnCard = document.querySelector(".btn-card");
+
+btnCard.addEventListener("click", function () {
+	const message = prompt("Type een chatbericht in");
+	container.innerHTML += `
+    <div class="card">${message}</div>
+    `;
+});
